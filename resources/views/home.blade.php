dcf0790caa68dd00f3951d7d8a24fe28af1d62ff
RudeGames <fs_boukhoulda@esi.dz>
2018-10-11 09:01:25 +0100

    Finished the homepage layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="zxx">
+@include('includes.head')
+<body class="shop home-page">
+
+	<!-- Side Navigation Menu -->
+	<aside class="side-navigation-wrapper enter-right" data-no-scrollbar data-animation="slide-in">
+		<div class="side-navigation-scroll-pane">
+			<div class="side-navigation-inner">
+				<div class="side-navigation-header">
+					<div class="navigation-hide side-nav-hide">
+						<a href="#">
+							<span class="icon-cancel medium"></span>
+						</a>
+					</div>
+				</div>
+				<nav class="side-navigation nav-block">
+					<ul>
+						<li class="current">
+							<a href="#" class="">Accueil</a>
+						</li>
+						<li>
+							<a href="#" class="">Association</a>
+						</li>
+						<li>
+							<a href="#" class="">Maladie</a>
+						</li>
+						<li>
+							<a href="#" class="">Services</a>
+						</li>
+						<li>
+							<a href="#" class="">Activités</a>
+						</li>
+						<li>
+							<a href="#" class="contains-sub-menu">Nouveautés</a>
+						</li>
+					</ul>
+				</nav>
+				<div class="side-navigation-footer">
+					<p class="copyright no-margin-bottom">&copy; 2017 ThemeMountain.</p>
+				</div>
+			</div>
+		</div>
+	</aside>
+	<!-- Side Navigation Menu End -->
+
+	<div class="wrapper reveal-side-navigation">
+		<div class="wrapper-inner">
+
+			<!-- Header -->
+			@include('includes.header')
+			<!-- Header End -->
+
+			<!-- Content -->
+			<div class="content clearfix">
+				<!-- Full Width Slider -->
+				<div class="section-block pt-0 pb-0">
+						<div id="fws-1" class="tm-slider-container full-width-slider" data-scale-under="1140" data-speed="1000">
+							<ul class="tms-slides">
+								<li class="tms-slide" data-image data-force-fit data-animation="slideLeftRight" data-overlay-bkg-color="#000000" data-overlay-bkg-opacity="0.3">
+									<div class="tms-content">
+										<div class="tms-content-inner center">
+											<div class="row">
+												<div class="column width-12">
+													<h2 class="color-white mb-0">Accepting Payments</h2>
+													<h2 class="color-white weight-semi-bold">Just got really, really easy</h2>
+													<p class="lead color-white">FaulknerPay is an app that enables anyone to send, receive, and pay for goods and services with the tap of a button.</p>
+													<a data-content="inline" data-aux-classes="tml-promotion-modal tml-padding-small tml-swap-exit-light height-auto rounded" data-toolbar="" data-modal-mode data-modal-width="1140" data-modal-animation="scaleIn" data-lightbox-animation="fadeIn" href="#promotion-modal" class="lightbox-link button rounded medium bkg-theme bkg-hover-theme color-white color-hover-white"
+													>
+													<span class="button-content">
+															<span class="icon-app-store medium left"></span>
+															<span>
+																<small>Aidez nous ^^</small>Donation
+															</span>
+														</span>
+													</a>
+													<!-- modal window -->
+													<div id="promotion-modal" class="hide">
+														
+															<!-- Hero 5 Promo Custom Section -->
+															<div class="section-block hero-5 hero-5-2 pb-mobile-20 show-media-column-on-mobile">
+																<div class="media-column width-5">
+																	<div class="media-overlay bkg-black opacity-01"></div>
+																</div>
+																<div class="row">
+																	<div class="column width-5 offset-6">
+																		<div class="hero-content split-hero-content">															
+																			<div class="hero-content-inner left horizon" data-animate-in="preset:slideInRightShort;duration:1000ms;delay:200ms;" data-threshold="0.6">
+																				<h3 class="mb-30">Now Only $30.00</h3>
+																				<p>Morbi nec ultrices tellus. Fusce id est quis orci faucibus congue. Aliquam erat volutpat. Phasellus tortor velit, ornare at ullamcorper at, ultrices et justo. Cum sociis natoque penatibus et magnis.</p>
+																				<div class="product-actions">
+																					<a href="#" class="button rounded medium bkg-theme bkg-hover-theme color-white color-hover-white add-to-cart-button medium">Add To Cart</a>
+																				</div>
+																			</div>
+																	</div>
+																</div>
+															</div>
+														</div>
+														<!-- Hero 5 Promo Custom Section End -->
+										
+													</div>
+													<!--  Modal End -->
+													<div class="clear"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<img data-src="images/test_slider.jpg" data-retina src="images/blank.png" alt=""/>
+								</li>
+								<li class="tms-slide" data-image data-force-fit data-animation="slideLeftRight" data-overlay-bkg-color="#000000" data-overlay-bkg-opacity="0.3">
+									<div class="tms-content">
+										<div class="tms-content-inner center">
+											<div class="row">
+												<div class="column width-12">
+														<h2 class="color-white mb-0">Accepting Payments</h2>
+															<h2 class="color-white weight-semi-bold">Just got really, really easy</h2>
+															<p class="lead color-white">FaulknerPay is an app that enables anyone to send, receive, and pay for goods and services with the tap of a button.</p>
+															<a data-content="inline" data-aux-classes="tml-promotion-modal tml-padding-small tml-swap-exit-light height-auto rounded" data-toolbar="" data-modal-mode data-modal-width="1140" data-modal-animation="scaleIn" data-lightbox-animation="fadeIn" href="#promotion-modal" class="lightbox-link button rounded medium bkg-theme bkg-hover-theme color-white color-hover-white"
+															>
+															<span class="button-content">
+																	<span class="icon-app-store medium left"></span>
+																	<span>
+																		<small>Aidez nous ^^</small>Donation
+																	</span>
+																</span>
+															</a>
+												</div>
+											</div>
+										</div>
+									</div>
+									<img data-src="images/test_slider2.jpg" data-retina src="images/blank.png" alt=""/>
+								</li>
+							</ul>
+						</div>
+				</div>
+				<!-- Full Width Slider -->
+			</div>
+			<!-- Content End -->
+
+			<!-- Content -->
+			<div class="content clearfix">
+
+				<!-- Intro Title Section 1 -->
+				<div class="section-block replicable-content bkg-grey-ultralight">
+					<div class="row flex">
+						<div class="column width-8 offset-2 left">
+							<center>
+							<h1>Sep Espoire</h1>
+							<p class="lead mb-10">Transformons ensemble l’avenir de la SEP</p>
+							</center>
+						</div>
+					</div>
+				</div>
+				<!-- Intro Title Section 1 End -->
+				<!-- Hero Slider -->
+				<div class="section-block hero-5 clear-height show-media-column-on-mobile bkg-gradient-white-wave">
+					<div class="row">
+						<div class="column width-5">
+							<div class="hero-content split-hero-content">
+								<div class="hero-content-inner left horizon" data-animate-in="preset:slideInRightShort;duration:1000ms;" data-threshold="0.5">
+									<h2 class="mb-30">Qui sommes nous?</h2>
+									<p>C’est une association faites pour les malades atteints de la sclérose en plaques. Créer en 2016. Ayant une vision, un butes et sur tous <span style="font-weight:600; color:#4594C7;">UN ESPOIR</span>.</p>
+									<p>Un de ses importants objectifs est de contribuer pour un avenir meilleur pour les malades atteints de la Sclérose en plaques.</p>
+									<a class="button medium rounded bkg-gradient-dark-night bkg-hover-theme color-white color-hover-white mb-0 mb-mobile-20 inline lightbox-link" data-lightbox-animation="fade">Association</a>	
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="media-column width-6 offset-6 window-height background-none">
+							<div class="tm-slider-container content-slider window-height" data-width="722" data-nav-arrows="false" data-nav-show-on-hover="false" data-auto-advance data-auto-advance-interval="4000" data-progress-bar="false" data-speed="1000" data-scale-under="0">
+								<ul class="tms-slides">
+									<li class="tms-slide" data-image data-as-bkg-image data-force-fit data-animation="scaleOut">
+										<img data-src="images/slider/slide-hero-half-2.jpg" data-retina src="images/blank.png" alt=""/>
+									</li>
+								</ul>
+							</div>
+					</div>
+				</div>
+				<!-- Hero Slider End -->
+				<!-- Hero Slider -->
+				<div class="section-block hero-5 clear-height show-media-column-on-mobile bkg-gradient-dark-night-header color-white">
+					<div class="media-column width-6 window-height background-none">
+						<div class="tm-slider-container content-slider window-height" data-width="722" data-nav-arrows="false" data-nav-show-on-hover="false" data-auto-advance data-auto-advance-interval="4000" data-progress-bar="false" data-speed="1000" data-scale-under="0">
+							<ul class="tms-slides">
+								<li class="tms-slide" data-image data-as-bkg-image data-force-fit data-animation="scaleOut">
+									<img data-src="images/slider/slide-hero-half-2.jpg" data-retina src="images/blank.png" alt=""/>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="row">
+						<div class="column width-5 offset-7">
+							<div class="hero-content split-hero-content">
+								<div class="hero-content-inner left horizon" data-animate-in="preset:slideInRightShort;duration:1000ms;" data-threshold="0.5">
+									<h2 class="mb-30">La Sep?</h2>
+									<p>La sclérose en plaques est actuellement considérée comme une maladie auto-immune qui s’attaque au système nerveux central (cerveau, moelle épinière et nerfs optiques).</p>
+									<p>C’est une maladie qui peut causer de grave conséquence si elle n’a pas été bien traité ou négliger. La sclérose en plaque ,il est temps de briser le tabou ,d’en parler et d’en mieux connaitre.</p>
+									<a class="button medium rounded bkg-gradient-white-wave-darker bkg-hover-theme color-hover-white mb-0 mb-mobile-20 inline lightbox-link" data-lightbox-animation="fade" style="color:#212325 !important;">Maladie</a>	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Hero Slider End -->
+			</div>
+			<!-- End content -->
+
+
+			<!-- Testimonial Slider -->
+			<div class="section-block">
+					<div class="row">
+						<div class="column width-12 center">
+							<h2 class="mb-20">Un mot du président et du secrétaire général</h2>
+						</div>
+					</div>
+					<div class="row full-width collapse">
+						<div class="column width-12 center">
+							<div class="tm-slider-container testimonial-slider" data-nav-dark>
+								<ul class="tms-slides">	
+									<li class="tms-slide" data-image>
+										<div class="tms-content-scalable">
+											<div class="row full-width no-margins">
+												<div class="column width-6 offset-3">
+													<blockquote class="avatar large center">
+														<span><img src="images/team/team-member-1-square.jpg" alt=""></span>
+														<p>Incredible work! Best creative experience ever; timely, well executed and one hell of the result. Would recommend Nietszche to anyone who is looking to revamp their identity.
+															<cite>John K. Riley - Harvest Goods.</cite>
+														</p>
+													</blockquote>
+												</div>
+											</div>
+										</div>
+									</li>
+									<li class="tms-slide" data-image>
+										<div class="tms-content-scalable">
+											<div class="row full-width no-margins">
+												<div class="column width-6 offset-3">
+													<blockquote class="avatar large center">
+														<span><img src="images/team/team-member-2-square.jpg" alt=""></span>
+														<p>A five star agency without doubt. Hire them!
+															<cite>John A. Adams - Repetition</cite>
+														</p>
+													</blockquote>
+												</div>
+											</div>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+			</div>
+			<!-- Testimonial Slider End -->
+
+			<!-- Logo Slider -->
+			<div class="section-block bkg-grey-ultralight">
+					<div class="row">
+						<div class="column width-12 center">
+							<h2 class="mb-20">Nos partenaires</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="column width-12">
+							<div class="tm-slider-container logo-slider pb-30" data-nav-arrows="false" data-nav-show-on-hover="false" data-nav-dark data-nav-keyboard="false" data-auto-advance data-auto-advance-interval="4000" data-progress-bar="false" data-pause-on-hover="false" data-carousel-visible-slides="5">
+								<ul class="tms-slides">	
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/froogle-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/squidbit-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/achillian-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/dropfile-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/berry-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/froogle-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/squidbit-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/achillian-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/dropfile-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+									<li class="tms-slide">
+										<div class="tms-content-scalable">
+											<img data-src="images/logos/berry-logo-grid.svg" src="images/blank.png" alt=""/>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+			</div>
+			<!-- Logo Slider End -->
+
+			<!-- Footer -->
+			@include('includes.footer')
+			<!-- Footer End -->
+
+		</div>
+	</div>
+
+	<!-- Js -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3JCAhNj6tVAO_LSb8M-AzMlidiT-RPAs"></script>
+	<script src="js/timber.master.min.js"></script>
+</body>
+</html>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,78 @@
+<header class="header header-absolute header-fixed-on-mobile" data-helper-in-threshold="200" data-helper-out-threshold="500" data-sticky-threshold="200" data-bkg-threshold="100" data-compact-threshold="100">
+    <div class="header-inner-top bkg-gradient-dark-night-header">
+        <div class="nav-bar">
+            <div class="row flex">
+                <div class="column width-8 nav-bar-inner v-align-middle left">
+                    <div>
+                        <p>Télephone: +213698162821 / Email: sepespoire@ass.dz</p>
+                    </div>
+                </div>
+                <div class="column width-4 nav-bar-inner v-align-middle right">
+                    <div>
+                        <ul class="social-list list-horizontal">
+                            <li><a href="#"><span class="icon-facebook small"></span></a></li>
+                            <li><a href="#"><span class="icon-twitter small"></span></a></li>
+                            <li><a href="#"><span class="icon-vimeo small"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-inner">
+        <div class="row nav-bar">
+            <div class="column width-12 nav-bar-inner">
+                <div class="row">
+                    <div class="column width-5 primary-nav-column">
+                        <nav class="navigation nav-block primary-navigation nav-right sub-menu-indicator">
+                            <ul>
+                                <li class="current">
+                                    <a href="index.html">Accueil</a>
+                                </li>
+                                <li class="">
+                                    <a href="about-style-one.html">Association</a>
+                                </li>
+                                <li class="">
+                                    <a href="about-style-one.html">Maladie</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="column width-2 logo-column">
+                        <div class="logo logo-center">
+                            <div class="logo-inner">
+                                <a href="index.html"><img src="images/logo-dark.png" alt="Faulkner Logo" /></a>
+                                <a href="index.html"><img src="images/logo.png" alt="Faulkner Logo" /></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column width-5 secondary-nav-column">
+                        <nav class="navigation nav-block primary-navigation nav-left sub-menu-indicator">
+                            <ul>
+                                <li class="">
+                                    <a href="portfolio-three-columns.html">Services</a>
+                                </li>
+                                <li class="">
+                                    <a href="blog-wide.html">Activités</a>
+                                </li>
+                                <li class="">
+                                    <a href="blog-wide.html">Nouveautés</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <nav class="navigation nav-block secondary-navigation nav-right">
+                            <ul>
+                                <li class="aux-navigation hide">
+                                    <!-- Aux Navigation -->
+                                    <a href="#" class="navigation-show side-nav-show nav-icon">
+                                        <span class="icon-menu"></span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
